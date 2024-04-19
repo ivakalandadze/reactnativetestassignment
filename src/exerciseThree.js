8c585a522fd2fd67823e9cd15120a5236049bba8
Luka Palelashvili <lukapalelashvili@gmail.com>
2024-04-19 13:37:40 +0400

    Update exerciseThree.js

--- src/exerciseThree.js
+++ src/exerciseThree.js
@@ -1,1 +1,22 @@
+const left = (string) => {
+  let a = string;
+  let b = [];
+  let c = "";
+  let d = [];
+  let e = [];
+  for (let i = 0; i < string.length; i++) {
+    b.push(string[i]);
+  }
+  for (let j = 0; j < b.length; j++) {
+    for (let k = 0; k < b.length; k++) {
+      c += b[k];
+    }
+    e.push(c);
+    d = b.shift();
+    b.push(d);
+    c = ""
+  }
+  return e;
+}
 
+console.log(left("abc"));
