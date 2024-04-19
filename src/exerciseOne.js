d2b36199a833c06af1a7c18c151d374be5143c05
Luka Palelashvili <lukapalelashvili@gmail.com>
2024-04-19 13:31:46 +0400

    Update exerciseOne.js

--- src/exerciseOne.js
+++ src/exerciseOne.js
@@ -1,1 +1,15 @@
+const array = [1,2,3,4,2,3]; 
 
+function array_move(arr, old_index, new_index) {
+    if (new_index >= arr.length) {
+        var k = new_index - arr.length + 1;
+        while (k--) {
+            arr.push(undefined);
+        }
+    }
+    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+    return arr; 
+};
+
+
+console.log(array_move([1,2,3,4,2,3]));
